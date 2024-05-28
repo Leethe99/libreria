35cff5f184f21726633c7eabe7ef707d903c67d7
Rafael Lee <r.lee@ukko.mx>
2024-05-28 00:54:33 -0600

    reservation

---
+++ libreria/Controllers/ReservationController.cs
@@ -0,0 +1,195 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using libreria.Models.dbModels;
+using libreria.Models;
+
+namespace libreria.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private readonly BookstoreManagerContext _context;
+
+        public ReservationController(BookstoreManagerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reservation
+        public async Task<IActionResult> Index()
+        {
+            var bookstoreManagerContext = _context.Reservations.Include(r => r.Book).Include(r => r.Status).Include(r => r.Store).Include(r => r.User);
+            return View(await bookstoreManagerContext.ToListAsync());
+        }
+
+        // GET: Reservation/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Reservations == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations
+                .Include(r => r.Book)
+                .Include(r => r.Status)
+                .Include(r => r.Store)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // GET: Reservation/Create
+        public IActionResult Create()
+        {
+            ViewData["BookId"] = new SelectList(_context.Books, "Id", "Id");
+            ViewData["StatusId"] = new SelectList(_context.ReservationStatuses, "Id", "Id");
+            ViewData["StoreId"] = new SelectList(_context.Stores, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Reservation/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,StoreId,BookId,Quantity,StatusId,CreatedAt")] ReservationHR reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                Reservation reservation1 = new Reservation
+                {
+                    UserId = reservation.UserId,
+                    StoreId = reservation.StoreId,
+                    BookId = reservation.BookId,
+                    Quantity = reservation.Quantity,
+                    StatusId = reservation.StatusId,
+                    CreatedAt = reservation.CreatedAt
+                };
+                _context.Reservations.Add(reservation1);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BookId"] = new SelectList(_context.Books, "Id", "Id", reservation.BookId);
+            ViewData["StatusId"] = new SelectList(_context.ReservationStatuses, "Id", "Id", reservation.StatusId);
+            ViewData["StoreId"] = new SelectList(_context.Stores, "Id", "Id", reservation.StoreId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", reservation.UserId);
+            return View(reservation);
+        }
+
+        // GET: Reservation/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Reservations == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+            ViewData["BookId"] = new SelectList(_context.Books, "Id", "Id", reservation.BookId);
+            ViewData["StatusId"] = new SelectList(_context.ReservationStatuses, "Id", "Id", reservation.StatusId);
+            ViewData["StoreId"] = new SelectList(_context.Stores, "Id", "Id", reservation.StoreId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", reservation.UserId);
+            return View(reservation);
+        }
+
+        // POST: Reservation/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,StoreId,BookId,Quantity,StatusId,CreatedAt")] Reservation reservation)
+        {
+            if (id != reservation.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reservation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReservationExists(reservation.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BookId"] = new SelectList(_context.Books, "Id", "Id", reservation.BookId);
+            ViewData["StatusId"] = new SelectList(_context.ReservationStatuses, "Id", "Id", reservation.StatusId);
+            ViewData["StoreId"] = new SelectList(_context.Stores, "Id", "Id", reservation.StoreId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", reservation.UserId);
+            return View(reservation);
+        }
+
+        // GET: Reservation/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Reservations == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations
+                .Include(r => r.Book)
+                .Include(r => r.Status)
+                .Include(r => r.Store)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // POST: Reservation/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Reservations == null)
+            {
+                return Problem("Entity set 'BookstoreManagerContext.Reservations'  is null.");
+            }
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation != null)
+            {
+                _context.Reservations.Remove(reservation);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReservationExists(int id)
+        {
+          return (_context.Reservations?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ libreria/Models/ReservationHR.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace libreria.Models
+{
+	public class ReservationHR
+	{
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int StoreId { get; set; }
+
+        public int BookId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int StatusId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
+
